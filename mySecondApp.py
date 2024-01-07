67696d409533a2573a4c42c7dd157d277460fbae
Giulianogoliver <giulianogoliver84@gmail.com>
2024-01-07 11:07:01 -0300

    finish second app

--- mySecondApp.py
+++ mySecondApp.py
@@ -13,5 +13,16 @@ seg_num = st.sidebar.number_input("Digite o segundo número:")
 st.sidebar.subheader("Operaçao")
 select_operation = st.sidebar.radio("Selecione a operaçao",("Adiçao", "Subtraçao"))
 
+btn_calc = st.button("Calcular")
 
+if btn_calc:
+    st.header("Operaçao escolhida")
+    st.subheader(select_operation)
+    st.header("Numeros selecionados")
+    st.subheader(f"O primeiro numero é: {prm_num}. O segundo numero é: {seg_num}.")
+    st.header("Resultado da operaçao")
+    if select_operation == "Adiçao":
+        result = prm_num + seg_num
+    else: result = prm_num - seg_num
+    st.subheader(f"O resultado da operaçao selecionada é igual a {result}")
 
